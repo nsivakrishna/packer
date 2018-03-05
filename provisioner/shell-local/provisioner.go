2ce1e5798e868570a7f6682de4c4b9e92a3c50b2
Megan Marsh <megan@hashicorp.com>
2018-03-05 09:54:21 -0800

    make bash call work on windows

--- provisioner/shell-local/provisioner.go
+++ provisioner/shell-local/provisioner.go
@@ -1,7 +1,10 @@
 package shell
 
 import (
+	"fmt"
+	"path/filepath"
 	"runtime"
+	"strings"
 
 	sl "github.com/hashicorp/packer/common/shell-local"
 	"github.com/hashicorp/packer/packer"
@@ -16,15 +19,42 @@ func (p *Provisioner) Prepare(raws ...interface{}) error {
 	if err != nil {
 		return err
 	}
+	convertPath := false
 	if len(p.config.ExecuteCommand) == 0 && runtime.GOOS == "windows" {
+		convertPath = true
 		p.config.ExecuteCommand = []string{
 			"bash",
 			"-c",
-			"\"{{.Vars}} {{.Script}}\"",
+			"{{.Vars}} {{.Script}}",
 		}
 	}
 
-	return sl.Validate(&p.config)
+	err = sl.Validate(&p.config)
+	if err != nil {
+		return err
+	}
+
+	if convertPath {
+		for index, script := range p.config.Scripts {
+			p.config.Scripts[index], err = convertToWindowsBashPath(script)
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
+
+func convertToWindowsBashPath(winPath string) (string, error) {
+	// get absolute path of script, and morph it into the bash path
+	winAbsPath, err := filepath.Abs(winPath)
+	if err != nil {
+		return "", fmt.Errorf("Error converting %s to absolute path: %s", winPath, err.Error())
+	}
+	winAbsPath = strings.Replace(winAbsPath, "\\", "/", -1)
+	winBashPath := strings.Replace(winAbsPath, "C:/", "/mnt/c/", 1)
+	return winBashPath, nil
 }
 
 func (p *Provisioner) Provision(ui packer.Ui, _ packer.Communicator) error {
